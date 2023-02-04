77de1b52e9ca12452211abc73499b93ab315dc89
a.bakharev <b.baha2011@yandex.ru>
2023-02-04 20:32:36 +0500

    Initial commit

---
+++ src/main/java/DTO/ValuteCursOnDate.java
@@ -0,0 +1,26 @@
+package DTO;
+
+import lombok.Data;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+public class ValuteCursOnDate {
+
+    @XmlElement(name = "Vname")
+    private String name;
+
+    @XmlElement(name = "Vnom")
+    private int nominal;
+
+    @XmlElement(name = "Vcurs")
+    private double course;
+
+    @XmlElement(name = "Vcode")
+    private String code;
+
+    @XmlElement(name = "VchCode")
+    private String chCode;
+}
